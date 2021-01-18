0c57333c88aab4e31874da28f77e7a2f36dac3c4
YordanStoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-18 10:27:46 +0200

    Add files via upload

---
+++ NumberInRange/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberInRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if (number >= -100 && number < 0)
+            {
+                Console.WriteLine("Yes");
+            }
+            else if (number <= 100 && number > 0)
+            {
+                Console.WriteLine("Yes");
+            }
+            else if (number == 0 || number > 100 || number < -100)
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
+
