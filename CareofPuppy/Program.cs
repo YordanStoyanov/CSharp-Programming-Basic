1368631410f7067278bbb231122bc75a970a3e3e
YordanStoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-18 10:39:36 +0200

    Add files via upload

---
+++ CareofPuppy/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareofPuppy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int food = int.Parse(Console.ReadLine());
+            int foodInGrams = food * 1000;
+            string eated = null;
+            int eatedFoodSum = 0;
+            int eatedFood = 0;
+            int change = 0;
+            while (true)
+            {
+                eated = Console.ReadLine();
+
+                if (eated == "Adopted")
+                {
+                    if (eatedFoodSum <= foodInGrams)
+                    {
+                        Console.WriteLine("Food is enough! Leftovers: {0} grams.", change);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Food is not enough. You need {0} grams more.", eatedFoodSum - foodInGrams);
+                    }
+                    break;
+                }
+                eatedFood = int.Parse(eated);
+                eatedFoodSum += eatedFood;
+                change = foodInGrams - eatedFoodSum;
+            }
+        }
+    }
+}
