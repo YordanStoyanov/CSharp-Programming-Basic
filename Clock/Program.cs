6286be565a846ffb99b97c8f16a2f58db12d8883
YordanStoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-18 10:14:45 +0200

    Add files via upload

---
+++ Clock/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int j = 0;
+            while (true)
+            {
+                for (i = 0; i <= 59; i++)
+                {
+                    Console.WriteLine("{0}:{1}", j, i);
+
+                    if (i == 59)
+                    {
+                        j++;
+                    }
+                    
+                    if (j == 24)
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
