0029e4f0174ddea6c0675853b19587a9bbd0b436
YordanStoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-18 10:27:03 +0200

    Add files via upload

---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int j = 1;
+            while (true)
+            {
+                for (i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine("{0} * {1} = {2}", j, i, i * j);
+
+                    if (i == 10)
+                    {
+                        j++;
+                    }
+
+                    if (j == 11)
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
