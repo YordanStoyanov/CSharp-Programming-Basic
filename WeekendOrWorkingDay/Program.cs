89ab96a57de1c147bcfd696099f91edd16722be2
YordanStoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-18 10:36:53 +0200

    Add files via upload

---
+++ WeekendOrWorkingDay/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeekendOrWorkingDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string weeksDay = Console.ReadLine();
+
+            if (weeksDay == "Monday" || weeksDay == "Tuesday" || weeksDay == "Wednesday" || weeksDay == "Thursday" || weeksDay == "Friday")
+            {
+                Console.WriteLine("Working day");
+            }
+            else if (weeksDay == "Saturday" || weeksDay == "Sunday")
+            {
+                Console.WriteLine("Weekend");
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
