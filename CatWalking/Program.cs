13627145bd44b47eed398a3a750e764cc195f4d9
YordanStoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-18 10:39:58 +0200

    Add files via upload

---
+++ CatWalking/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatWalking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min = int.Parse(Console.ReadLine());
+            int numberOfWalking = int.Parse(Console.ReadLine());
+            int calPedDay = int.Parse(Console.ReadLine());
+            int walkinePedDay = min * numberOfWalking;
+            int combCalPedDay = walkinePedDay * 5;
+            int fiftheenPerCent = (calPedDay * 50) / 100;
+            if (combCalPedDay > fiftheenPerCent)
+            {
+                Console.WriteLine("Yes, the walk for your cat is enough. Burned calories per day: {0}.", combCalPedDay); 
+            }
+            else
+            {
+                Console.WriteLine("No, the walk for your cat is not enough. Burned calories per day: {0}.", combCalPedDay);
+            }
+        }
+    }
+}
