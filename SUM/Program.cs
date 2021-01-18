b7be285ec27df5d7aa20bb5044dfb6869f4205a5
YordanStoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-18 10:33:41 +0200

    Add files via upload

---
+++ SUM/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SUM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int sum = 0;
+            while (true)
+            {
+                if (sum < num)
+                {
+                    int num1 = int.Parse(Console.ReadLine());
+                    sum += num1; 
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
