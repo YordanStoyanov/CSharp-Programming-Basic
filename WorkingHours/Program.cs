6b70bfafb4843303e8de4b98e3534fab9fb132b8
YordanStoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-18 10:37:11 +0200

    Add files via upload

---
+++ WorkingHours/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingHours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            string weeksDay = Console.ReadLine();
+
+            if (number >= 10 && number <= 18)
+            {
+                if (weeksDay == "Monday" || weeksDay == "Tuesday" || weeksDay == "Wednesday" || weeksDay == "Thursday" || weeksDay == "Friday" || weeksDay == "Saturday")
+                {
+                    Console.WriteLine("open");
+                }
+                else if (weeksDay == "Sunday")
+                {
+                    Console.WriteLine("closed");
+                }
+            }
+            else if (number < 10 || number > 18)
+            {
+                    Console.WriteLine("closed");
+            }
+        }
+    }
+}
