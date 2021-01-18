fe3f5a67da2f7e2d5c2578b5ad7799acdc35c7b9
YordanStoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-18 10:28:55 +0200

    Add files via upload

---
+++ NumberSequence/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] numbers = new int[n];
+            int min = numbers[0];
+            int max = numbers[1];
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+                if (max < numbers[i])
+                {
+                    max = numbers[i];
+                }
+                if (min > numbers[i])
+                {
+                    min = numbers[i];
+                }
+            }
+
+            Console.WriteLine("Max number: {0}", max);
+            Console.WriteLine("Min number: {0}", min);
+        }
+    }
+}
