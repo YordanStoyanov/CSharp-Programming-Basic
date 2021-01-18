d86be7aa27169ba4fe5d4cc00139010113d9a17d
YordanStoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-18 10:50:29 +0200

    Add files via upload

---
+++ EasterShop/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasterShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfEggs = int.Parse(Console.ReadLine());
+            int sold = 0;
+            
+            while (true)
+            {
+                string buyFill = Console.ReadLine();
+
+                if (buyFill == "Close")
+                {
+                    Console.WriteLine("Store is closed!");
+                    Console.WriteLine("{0} eggs sold.", sold);
+                    break;
+                }
+
+                int buyOrFill = int.Parse(Console.ReadLine());
+
+                if (buyFill == "Buy")
+                {
+                    if (numberOfEggs < buyOrFill)
+                    {
+                        Console.WriteLine("Not enough eggs in store!");
+                        Console.WriteLine("You can buy only {0}.", numberOfEggs);
+                        break;
+                    }
+                    else
+                    {
+                        numberOfEggs -= buyOrFill;
+                        sold += buyOrFill;
+                    }
+                }
+                else if (buyFill == "Fill")
+                {
+                    numberOfEggs += buyOrFill;
+                }
+                
+            }
+        }
+    }
+}
