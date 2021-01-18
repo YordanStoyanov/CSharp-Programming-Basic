589d19c97f72e2c4d46b59ae48b0fadaa58c837e
YordanStoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-18 10:31:19 +0200

    Add files via upload

---
+++ Passrod/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Passrord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string user = Console.ReadLine();
+            string pass = Console.ReadLine();
+
+            string input = Console.ReadLine();
+
+            while (input != pass)
+            {
+                input = Console.ReadLine();
+            }
+            Console.WriteLine("Welcome {0}!", user);
+        }
+    }
+}
