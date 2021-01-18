e20d767530a22f72d83152e97252c127e9091763
YordanStoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-18 10:57:55 +0200

    Add files via upload

---
+++ SummerShoping/SummerShoping.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SummerShoping
+{
+    public class SummerShoping
+    {
+        public static void Main()
+        {
+            //цената на чадъра е две трети от цената на хавлията
+            //цената на джапанките е 75% от тази на плажния чадър
+            // Плажната чанта струва една трета от сумата за джапанките и хавлията взети заедно
+            //процентна отстъпка от общата сума на покупката
+            decimal annysBudjet = decimal.Parse(Console.ReadLine());//бюджетът на ани
+            decimal beachTowel = decimal.Parse(Console.ReadLine());//цена на плажната хавля
+            decimal percentDiscount = decimal.Parse(Console.ReadLine());//отстъпка от общата цена
+
+
+        }
+    }
+}
