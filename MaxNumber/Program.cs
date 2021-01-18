7d29baa59d2203949df1799f6108273c241a8efc
YordanStoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-18 10:26:15 +0200

    Add files via upload

---
+++ MaxNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int smallNumber = int.MaxValue;
+            while (true)
+            {
+                string num = Console.ReadLine();
+                if (num == "Stop")
+                {
+                    break;
+                }
+                int number = int.Parse(num);
+                if (smallNumber > number)
+                {
+                    smallNumber = number;
+                }
+            }
+            Console.WriteLine(smallNumber);
+        }
+    }
+}
