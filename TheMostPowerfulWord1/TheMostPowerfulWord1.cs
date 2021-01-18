a98232a68248da87c7dc4fb255459b5142be4970
YordanStoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-18 10:59:32 +0200

    Add files via upload

---
+++ TheMostPowerfulWord1/TheMostPowerfulWord1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheMostPowerfulWord1
+{
+    public class TheMostPowerfulWord1
+    {
+        public static void Main()
+        {
+            int sum = 0;
+            string[] str = {"The"};
+            int l = str.Length;
+            for (int i = 0; i < l; i++)
+            {
+
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
