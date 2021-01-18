71da928fc01b00df8beef5fac3b9d2c93d168441
YordanStoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-18 11:04:13 +0200

    Add files via upload

---
+++ HoneyHarvest/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoneyHarvest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string typeFlower = Console.ReadLine();
+            int numberOfFlower = int.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            double total = 0;
+            double result = 0;
+
+            if (typeFlower == "Sunflower")
+            {
+                if (season == "Spring")
+                {
+                    total = 10;
+                    result = (double)numberOfFlower * total;
+                }
+                else if (season == "Summer")
+                {
+                    total = 8;
+                    result = (double)numberOfFlower * total + ((double)numberOfFlower * total * (double)10) / (double)100;
+                }
+                else if (season == "Autumn")
+                {
+                    total = 12;
+                    result = numberOfFlower * total - ((double)numberOfFlower * total * (double)5) / (double)100;
+                }
+            } 
+            else if (typeFlower == "Daisy")
+            {
+                if (season == "Spring")
+                {
+                    total = 12;
+                    result = numberOfFlower * total + ((double)numberOfFlower * total * (double)10) / (double)100;
+
+                }
+                else if (season == "Summer")
+                {
+                    total = 8;
+                    result = numberOfFlower * total + ((double)numberOfFlower * total * (double)10) / (double)100;
+                }
+                else if (season == "Autumn")
+                {
+                    total = 6;
+                    result = numberOfFlower * total - ((double)numberOfFlower * total * (double)5) / (double)100;
+                }
+            }
+            else if (typeFlower == "Lavender")
+            {
+                if (season == "Spring")
+                {
+                    total = 12;
+                    result = numberOfFlower * total;
+                }
+                else if (season == "Summer")
+                {
+                    total = 8;
+                    result = numberOfFlower * total + ((double)numberOfFlower * total * (double)10) / (double)100;
+                }
+                else if (season == "Autumn")
+                {
+                    total = 6;
+                    result = numberOfFlower * total - ((double)numberOfFlower * total * (double)5) / (double)100;
+                }
+            }
+            else if (typeFlower == "Mint")
+            {
+                if (season == "Spring")
+                {
+                    total = 10;
+                    result = numberOfFlower * total + ((double)numberOfFlower * total * (double)10) / (double)100;
+                }
+                else if (season == "Summer")
+                {
+                    total = 12;
+                    result = numberOfFlower * total + ((double)numberOfFlower * total * (double)10) / (double)100;
+                }
+                else if (season == "Autumn")
+                {
+                    total = 6;
+                    result = numberOfFlower * total - ((double)numberOfFlower * total * (double)5) / (double)100;
+                }
+            }
+
+            Console.WriteLine("Total honey harvested: {0:f2}", result);
+
+        }
+    }
+}
