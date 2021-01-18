f4478995412b67bec9a06148c42c4373b8e68875
YordanStoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-18 10:13:28 +0200

    Add files via upload

---
+++ BirthdayParty/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirthdayParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal theHallRent = decimal.Parse(Console.ReadLine());
+
+            decimal cake = (theHallRent * 20m) / 100m;
+            decimal drinks = cake - (cake * 45m) / 100m;
+            decimal animator = theHallRent / 3m;
+            decimal result = theHallRent + cake + drinks + animator;
+            Console.WriteLine(result);
+        }
+    }
+}
