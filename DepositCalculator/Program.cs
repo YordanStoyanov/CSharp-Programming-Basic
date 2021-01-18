372707f61685d9f0be48a5619fa5bbe5a69e385e
YordanStoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-18 10:16:04 +0200

    Add files via upload

---
+++ DepositCalculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DepositCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal depositSum = decimal.Parse(Console.ReadLine());
+            int depositPeriod = int.Parse(Console.ReadLine());
+            double AnnualInterestRate = double.Parse(Console.ReadLine());
+
+            decimal sum = depositSum + (decimal)depositPeriod * (((depositSum * (decimal)AnnualInterestRate / 100m) / 12m));
+            Console.WriteLine("{0:f2}", sum);
+        }
+    }
+}
