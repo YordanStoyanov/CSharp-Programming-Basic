b7a5053b7882d30aad4e429139e63694e8ab2dc2
YordanStoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-18 10:31:47 +0200

    Add files via upload

---
+++ PersonalTitles/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalTitles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            string sex = Console.ReadLine();
+            if (sex == "m")
+            {
+                if (number >= 16)
+                {
+                    Console.WriteLine("Mr.");
+                }
+                else if (number < 16)
+                {
+                    Console.WriteLine("Master");
+                }
+            }
+            else if (sex  == "f")
+            {
+                if (number >= 16)
+                {
+                    Console.WriteLine("Ms.");
+                }
+                else if (number < 16)
+                {
+                    Console.WriteLine("Miss");
+                }
+            }
+        }
+    }
+}
