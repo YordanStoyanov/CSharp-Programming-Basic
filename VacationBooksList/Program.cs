db001d64838df4b7fce1119534e0edb815ba0e70
YordanStoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-18 10:36:31 +0200

    Add files via upload

---
+++ VacationBooksList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VacationBooksList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPages = int.Parse(Console.ReadLine());
+            double numberOfPagesPecHour = double.Parse(Console.ReadLine());
+            int numberOfDaysForABook = int.Parse(Console.ReadLine());
+
+            double numberOfHour = (double)numberOfPages / numberOfPagesPecHour;
+            double numberOfHourPecDay = numberOfHour / (double)numberOfDaysForABook;
+            Console.WriteLine("{0}", numberOfHourPecDay);
+        }
+    }
+}
