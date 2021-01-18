bf31490b0b92160cb5af7c1735b7ba7a743f031e
YordanStoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-18 10:34:50 +0200

    Add files via upload

---
+++ SumNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int num = int.Parse(Console.ReadLine());
+        int result = 0;
+        for (int i = 0; i < num; i++)
+        {
+            int sum = int.Parse(Console.ReadLine());
+            result += sum;
+        }
+
+        Console.WriteLine(result);
+    }
+}
+
