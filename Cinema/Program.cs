2ac823d5b6922f2f1bb7f6c4c62a2a9f4553f3fc
YordanStoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-18 10:13:51 +0200

    Add files via upload

---
+++ Cinema/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string typeTicket = Console.ReadLine();
+            int rRows = int.Parse(Console.ReadLine());
+            int cCows = int.Parse(Console.ReadLine());
+            decimal ticketPrice = 0m;
+            if (typeTicket == "Premiere")
+            {
+                ticketPrice = 12.00m;
+            }
+            else if (typeTicket == "Normal")
+            {
+                ticketPrice = 7.50m;
+            }
+            else if (typeTicket == "Discount")
+            {
+                ticketPrice = 5.00m;
+            }
+            decimal result = ticketPrice * (decimal)rRows * (decimal)cCows;
+            Console.WriteLine("{0:f2} leva", result);
+        }
+    }
+}
