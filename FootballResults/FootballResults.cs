6b241b32066f577d5f3d0293ae5204fd171893da
YordanStoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-18 10:51:29 +0200

    Add files via upload

---
+++ FootballResults/FootballResults.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballResults
+{
+    public class FootballResults
+    {
+        public static void Main()
+        {
+
+                int firstGoal = int.Parse(Console.ReadLine());
+                int secontGoal = int.Parse(Console.ReadLine());
+                int wons = 0;
+                int losses = 0;
+                int drawn = 0;
+                if (firstGoal > secontGoal)
+                {
+                    wons++;
+                }
+                else if (firstGoal < secontGoal)
+                {
+                    losses++;
+                }
+                else if (firstGoal == secontGoal)
+                {
+                    drawn++;
+                }
+                Console.WriteLine("Team won {0} games.", wons);
+                Console.WriteLine("Team lost {0} games.", losses);
+                Console.WriteLine("Drawn games: {0}", drawn);
+        }
+    }
+}
