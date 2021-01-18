d0756972ba94833bf2730137b5a3b1f2d9ab1bd9
YordanStoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-18 10:55:10 +0200

    Add files via upload

---
+++ PoolDay/PoolDay.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoolDay
+{
+    public class PoolDay
+    {
+        public static void Main()
+        {
+            int numberOfPeople = int.Parse(Console.ReadLine());
+            decimal entranceFeeOfPeople = decimal.Parse(Console.ReadLine());
+            decimal priceOfADeckChair = decimal.Parse(Console.ReadLine());
+            decimal priceForUmbrella = decimal.Parse(Console.ReadLine());
+
+            decimal totalFee = (decimal)numberOfPeople * entranceFeeOfPeople;
+            decimal totalDeckChair = Math.Ceiling((decimal)numberOfPeople * (decimal)(75/100m)) * priceOfADeckChair;
+            decimal totalFeeForUmbprella = Math.Ceiling((decimal)numberOfPeople * (decimal)(50/100m)) * priceForUmbrella;
+            decimal result = totalFee + totalDeckChair + totalFeeForUmbprella;
+            Console.WriteLine("{0:f2} lv.", result);
+        }
+    }
+}
