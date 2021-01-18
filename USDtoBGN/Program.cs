f5ff19736bf5859cd998959c2d1fc5c293f3aa15
YordanStoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-18 10:36:12 +0200

    Add files via upload

---
+++ USDtoBGN/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace USDtoBGN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal usd = decimal.Parse(Console.ReadLine());
+            decimal usdToBg = 1.79549m;
+            decimal toBg = usd * usdToBg;
+            Console.WriteLine(toBg);
+
+        }
+    }
+}
