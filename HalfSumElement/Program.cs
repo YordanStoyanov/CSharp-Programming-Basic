657703afa30005900606168100728ba1c40d9418
YordanStoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-18 10:20:29 +0200

    Add files via upload

---
+++ HalfSumElement/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace NumberSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] numbers = new int[n];
+            int max = numbers[0];
+            int min = numbers[1];
+            int sumMin = 0;
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+
+                if (max < numbers[i])
+                {
+                    max = numbers[i];
+                }
+
+                if (min > numbers[i])
+                {
+                    min += numbers[i];
+                }
+
+                if (min < max)
+                {
+                    sumMin += min;
+                }
+            }
+
+            Console.WriteLine(max);
+            Console.WriteLine(sumMin);
+        }
+    }
+}
