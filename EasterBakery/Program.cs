c8b2a1612bee7bcd3053276e4eaaee4f26763ca8
YordanStoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-18 10:44:51 +0200

    Add files via upload

---
+++ EasterBakery/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasterBakery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal thePriceOfFlourPerKilogram = decimal.Parse(Console.ReadLine());
+            double numberOfKgOfFlour = double.Parse(Console.ReadLine());
+            double numberOfKgOfSugar = double.Parse(Console.ReadLine());
+            int numberOfAShellOfEggs = int.Parse(Console.ReadLine());
+            int numberOfPackOfYeast = int.Parse(Console.ReadLine());
+
+            decimal thePricePerKilogramOfSugar = thePriceOfFlourPerKilogram - (thePriceOfFlourPerKilogram * 25m) / 100m;
+            decimal thePriceOfAShellOfEggs = thePriceOfFlourPerKilogram + (thePriceOfFlourPerKilogram * 10m) / 100m;
+            decimal thePriceOfOnePackOfYeast = thePricePerKilogramOfSugar - (thePricePerKilogramOfSugar * 80m) / 100m;
+            decimal result = thePricePerKilogramOfSugar + thePriceOfAShellOfEggs + thePriceOfOnePackOfYeast;
+        }
+    }
+}
