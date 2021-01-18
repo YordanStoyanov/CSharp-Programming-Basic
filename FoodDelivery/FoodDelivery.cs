a807c424684d160fac55bf429bb76572fae5ad1a
YordanStoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-18 10:51:09 +0200

    Add files via upload

---
+++ FoodDelivery/FoodDelivery.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodDelivery
+{
+    public class FoodDelivery
+    {
+        public static void Main()
+        {
+            int NumberOfChickenMenus = int.Parse(Console.ReadLine());
+            int NumberOfFishMenus = int.Parse(Console.ReadLine());
+            int NumberOfVegetarianMenus = int.Parse(Console.ReadLine());
+            decimal chickenMenus = 10.35m;
+            decimal fishMenus = 12.40m;
+            decimal vegetarianMenus = 8.15m;
+            decimal total = (decimal)NumberOfChickenMenus * chickenMenus + (decimal)NumberOfFishMenus * fishMenus + (decimal)NumberOfVegetarianMenus * vegetarianMenus;
+            decimal disert = (total * 20m) / 100m;
+            decimal delivery = 2.5m;
+            decimal result = total + disert + delivery;
+            Console.WriteLine("Total: {0:f2}", result);
+        }
+    }
+}
