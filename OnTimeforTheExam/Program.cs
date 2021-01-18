c303e4bf0c65405f6ca0d74121af10ba1132489f
YordanStoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-18 10:30:29 +0200

    Add files via upload

---
+++ OnTimeforTheExam/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnTimeforTheExam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int examsTime = int.Parse(Console.ReadLine());
+            int examsMin = int.Parse(Console.ReadLine());
+            int arrivalTime = int.Parse(Console.ReadLine());
+            int arrivalMin = int.Parse(Console.ReadLine());
+
+            if (examsTime >= arrivalTime)
+            {
+                if (examsMin >= arrivalMin)
+                {
+                    
+                }
+            }
+            else if (examsTime < arrivalTime)
+            {
+                
+            }
+        }
+    }
+}
