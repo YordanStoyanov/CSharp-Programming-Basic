0d697329ffe9adddc0404272901545d15f783be9
YordanStoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-18 10:14:20 +0200

    Add files via upload

---
+++ CinemaTicket/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaTicket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string weeksDay = Console.ReadLine();
+
+            if (weeksDay == "Monday")
+            {
+                Console.WriteLine(12);
+            }
+            else if (weeksDay == "Tuesday")
+            {
+                Console.WriteLine(12);
+            }
+            else if (weeksDay == "Wednesday")
+            {
+                Console.WriteLine(14);
+            }
+            else if (weeksDay == "Thursday")
+            {
+                Console.WriteLine(14);
+            }
+            else if (weeksDay == "Friday")
+            {
+                Console.WriteLine(12);
+            }
+            else if (weeksDay == "Saturday")
+            {
+                Console.WriteLine(16);
+            }
+            else if (weeksDay == "Sunday")
+            {
+                Console.WriteLine(16);
+            }
+        }
+    }
+}
