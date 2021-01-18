ac71fd83128118044493cd278f69c2c42861cc2a
YordanStoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-18 10:28:10 +0200

    Add files via upload

---
+++ NumberPyramid/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberPyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int row = 0;
+            int col = 0;
+            int current = 0;
+            for (row = 1; row <= num; row++)
+            {
+                for (col = 1; col <= row; col++)
+                {
+                    current++;
+                    Console.Write(current + " ");
+                    
+                    if (current >= num)
+                    {
+                        break;
+                    }
+                }
+                if (current >= num)
+                {
+                    break;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
