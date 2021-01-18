2992d553adb202d7433723aff9734eac1bd6c702
YordanStoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-18 10:53:48 +0200

    Add files via upload

---
+++ MovieRatings/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRatings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int movieNumbers = int.Parse(Console.ReadLine());
+            string movieName = null;
+            double movieRatings = 0;
+            double biggestRating = 0;
+            double smallestRating = 0;
+            double middleRating = 0;
+            string movie1 = null;
+            string movie2 = null;
+                for (int i = 1; i <= movieNumbers; i++)
+                {
+                    movieName = Console.ReadLine();
+                    movieRatings = double.Parse(Console.ReadLine());
+
+                    if (biggestRating < movieRatings)
+                    {
+                        biggestRating = movieRatings;
+                        movie1 = movieName;
+                    }
+                    
+                    if (biggestRating > movieRatings)
+                    {
+                        smallestRating = movieRatings;
+                        movie2 = movieName;
+                    }
+
+                    middleRating += movieRatings;
+                }
+                Console.WriteLine("{0} is with highest rating: {1:f1}", movie1, biggestRating);
+                Console.WriteLine("{0} is with lowest rating: {1:f1}", movie2, smallestRating);
+                Console.WriteLine("Average rating: {0:f1}", middleRating / movieNumbers);
+        }
+    }
+}
