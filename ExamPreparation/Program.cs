6548cceab6bf0339081709d9ba0ccb43a9e97173
YordanStoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-18 10:17:25 +0200

    Add files via upload

---
+++ ExamPreparation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamPreparation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberUnsatisfactoryAssessments = int.Parse(Console.ReadLine());
+
+            while (true)
+            {
+                string task = Console.ReadLine();
+                int rating = int.Parse(Console.ReadLine());
+                if (task == "Enough")
+                {
+                    
+                }
+            }
+
+            Console.WriteLine("Average score: {средна оценка}");
+            Console.WriteLine("Number of problems: {броя на всички задачи}");
+            Console.WriteLine("Last problem: {името на последната задача}");
+            Console.WriteLine("You need a break, {брой незадоволителни оценки} poor grades.");
+
+        }
+    }
+}
