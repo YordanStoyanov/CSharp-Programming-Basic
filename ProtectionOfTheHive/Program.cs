6b56b9738faf6b772c98ec4d5f037dca8909446c
YordanStoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-18 11:04:39 +0200

    Add files via upload

---
+++ ProtectionOfTheHive/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtectionOfTheHive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfBees = int.Parse(Console.ReadLine());
+            int health = int.Parse(Console.ReadLine());
+            int attack = int.Parse(Console.ReadLine());
+            int bearAttack = numberOfBees - attack;
+            int beesConterAttack = health - bearAttack * 5;
+            int bearAttackAgein = bearAttack - attack;
+            int bearDeathOrLive = beesConterAttack - bearAttackAgein * 5;
+            if (bearDeathOrLive < 0)
+            {
+                Console.WriteLine("Beehive won! Bees left {0}.", bearAttackAgein);
+            }
+            
+            if (bearAttackAgein < 100  && beesConterAttack > 0)
+            {
+                Console.WriteLine("The bear stole the honey! Bees left {0}.", bearAttackAgein);
+            }
+            else if (beesConterAttack < 0)
+            {
+                Console.WriteLine("The bear stole the honey! Bees left {0}.", 0);
+            }
+            
+        }
+    }
+}
