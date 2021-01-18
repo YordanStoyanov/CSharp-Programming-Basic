91df3adaa7b7b96e1611d1093ac93d0569b35880
YordanStoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-18 10:39:01 +0200

    Add files via upload

---
+++ BasketballEquipment/BasketballEquipment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasketballEquipment
+{
+    public class BasketballEquipment
+    {
+        public static void Main()
+        {
+            decimal sumPerYear = decimal.Parse(Console.ReadLine());
+            decimal baskShoes = sumPerYear - sumPerYear * (decimal)0.4;
+            decimal baskClotes = baskShoes - baskShoes * (decimal)0.2;
+            decimal baskBoll = baskClotes * (decimal)0.25;
+            decimal baskAcces = baskBoll * (decimal)0.2;
+            decimal totalSum = sumPerYear + baskShoes + baskClotes + baskBoll + baskAcces;
+            Console.WriteLine("{0:f2}", totalSum);
+        }
+    }
+}
