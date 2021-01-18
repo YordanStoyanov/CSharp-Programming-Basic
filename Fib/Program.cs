08d8d8c19b283ff214fdc546ebca4765f1fb36df
YordanStoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-18 10:17:48 +0200

    Add files via upload

---
+++ Fib/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fib
+{
+    class Program
+    {
+        public int n;
+        public static int Fin(int n)
+        {
+            if (n <= 2)
+            {
+                return 1;
+            }
+            return Fib(n - 1) + Fib(n - 2);
+        }
+    }
+}
